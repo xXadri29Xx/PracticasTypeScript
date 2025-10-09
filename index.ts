2610d9acf0936aaa276e59f7c23e90933762eec9
xXadri29Xx <adrian29perez@gmail.com>
2025-10-09 15:00:37 +0200

    Implementación de Ejercicio 1

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log()
+function factorialRecursivo(n: number): number {
+  if(n === 0 || n === 1){
+    return 1;
+  }
+  return (n * factorialRecursivo(n-1));
+}
+ 
+const numero = 5;
+const resultadoFactorial = factorialRecursivo(numero);
+console.log(`El factorial de ${numero} es: ${resultadoFactorial}`); 
